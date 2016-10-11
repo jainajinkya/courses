dfb043e19dea860115892855654c5b96434100f9
labmember <labmember@kerouac.csres.utexas.edu>
2016-10-11 18:47:09 -0500

    RL/Asgn 2: code working but debuggin left

--- RL/Asgn2/sarsa_q_learning.py
+++ RL/Asgn2/sarsa_q_learning.py
@@ -7,7 +7,8 @@ from matplotlib import cm
 
 class grid_world:
 	def __init__(self):
-		self.state = np.zeros((4,12))
+		self.state = np.zeros((5,12))
+		self.t_state = [0,11]
 		# self.actions = ['r','l','u','d']
 
 	def state_update(self, cur_state, action):
@@ -15,26 +16,27 @@ class grid_world:
 		n_col = np.shape(self.state)[1]
 
 		new_state = cur_state
+		## action_set = ['r', 'l', 'u', 'd']
 
-		if action == 'r':
+		if action == 0:
 			if cur_state[1]< (n_col-1):
 				new_state[1] = cur_state[1] + 1
 			else:
 				new_state[1] = cur_state[1]
 
-		if action == 'l':
+		if action == 1:
 			if cur_state[1] > 0:
 				new_state[1] = cur_state[1] - 1
 			else:
 				new_state[1] = cur_state[1]
 
-		if action == 'u':
+		if action == 2:
 			if cur_state[0] < (n_row-1):
 				new_state[0] = cur_state[0] + 1
 			else:
 				new_state[0] = cur_state[0]
 
-		if action == 'd':
+		if action == 3:
 			if cur_state[0] > 0:
 				new_state[0] = cur_state[0] - 1
 			else:
@@ -44,13 +46,14 @@ class grid_world:
 
 
 	def r_function(self,cur_state):
-		cliff = [[0,i] for i in range(1,np.shape(self.state)[0]-2)]
-
-		reward = -1
+		cliff = [[0,i] for i in range(1,np.shape(self.state)[1]-1)]
+		reward = -1.
+		if cur_state == self.t_state:
+			reward = 50.
 
 		for i in range(len(cliff)):
 			if cur_state == cliff[i]:
-				reward = -100
+				reward = -100.
 				break
 
 		return reward
@@ -58,65 +61,88 @@ class grid_world:
 
 class control():
 	def e_greedy(self, cur_state,q_value):
-		eps = 0.1
+		eps = 0.2
 
 		p = rand.rand()
-		if p<0.1: 
+		if p<eps: 
 			action = rand.randint(0,4)
 		else:
 			action = np.argmax(q_value[cur_state[0]][cur_state[1]])
 
-		if action == 0:
-			action = 'r'
-		if action == 1:
-			action = 'l'
-		if action == 2:
-			action = 'u'
-		if action == 3:
-			action = 'd'
+		# if action == 0:
+		# 	action = 'r'
+		# if action == 1:
+		# 	action = 'l'
+		# if action == 2:
+		# 	action = 'u'
+		# if action == 3:
+		# 	action = 'd'
 
 		return action
-
-	# def policy(self):
 
 if __name__ == '__main__':
 	world = grid_world()
 	ctrl = control()
 	size_world = np.shape(world.state)
 	n_actions = 4
+	n_eps = 500
+	alpha = 0.2
+	disc = 1.0
+	
+	q_value = rand.rand(size_world[0],size_world[1],n_actions) # action_set = ['r','l','u','d']
+	# max_q_value = np.zeros((size_world[0],size_world[1]))
+	final_policy = []
 
-	cur_state = [3,0]
-	q_value = np.zeros((size_world[0],size_world[1],n_actions)) # action_set = ['r','l','u','d']
-	max_q_value = np.zeros((size_world[0],size_world[1]))
+	for ep in range(n_eps):
+		cur_state = [0,0]
+		counter = 0
 
-	for i in range(10):
-		# action = 'd'
-		action = ctrl.e_greedy(cur_state,q_value)
-		print "(state, action) = ", cur_state, action
-		
-		new_state = world.state_update(cur_state,action)
-		reward = world.r_function(new_state)
+		while(cur_state != world.t_state and counter<1000):
+			action = ctrl.e_greedy(cur_state,q_value)
+			new_state = world.state_update(cur_state,action)
+			reward = world.r_function(new_state)
+			new_action = ctrl.e_greedy(new_state,q_value)
 
-		q_value[new_state[0],new_state[1],0] = reward
-		if reward == -100:
-			new_state = [0,0]
+			## SARSA update
+			q_value[cur_state[0],cur_state[1],action] = q_value[cur_state[0],cur_state[1],action] + alpha*(reward + disc*q_value[new_state[0],new_state[1],action] - q_value[cur_state[0],cur_state[1],action])
 
-		cur_state = new_state
+			if reward == -100.:
+				new_state = [0,0] ## Restarting
+
+			cur_state = new_state
+			counter = counter + 1
+		# print "counter = ", counter
+	
+	# print q_value
 
 	for i in range(np.shape(q_value)[0]):
 		for j in range(np.shape(q_value)[1]):
-			max_q_value[i][j] = min(q_value[i][j])
+			# max_q_value[i][j] = max(q_value[i][j])
+			# print "q_value[i][j]", np.argmax(q_value[i][j])
+			final_policy = final_policy + [np.argmax(q_value[i][j])]
 
+	######## Final Trajectory
+	print "q_value =", q_value
+
+	cur_state = [0, 0]
+	for k in range(len(final_policy)):
+		cur_state = world.state_update(cur_state,final_policy[k])
+		print "cur_state = ", cur_state
+		# print "route =", r
+		
+
+	
 
 	# Plot reward function
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='3d')
-	X = np.arange(0, size_world[1], 1)
-	Y = np.arange(0, size_world[0], 1)
-	X, Y = np.meshgrid(X, Y)
-	surf = ax.plot_surface(X, Y, max_q_value, rstride=1, cstride=1, cmap=cm.coolwarm,
-			linewidth=0, antialiased=False)
-	plt.show()
+	# fig = plt.figure()
+
+	# ax = fig.add_subplot(111, projection='3d')
+	# X = np.arange(0, size_world[1], 1)
+	# Y = np.arange(0, size_world[0], 1)
+	# X, Y = np.meshgrid(X, Y)
+	# surf = ax.plot_surface(X, Y, max_q_value, rstride=1, cstride=1, cmap=cm.coolwarm,
+	# 		linewidth=0, antialiased=False)
+	# plt.show()
 
 
 
