bb499b344ffed10eda1c8bc60b3dec458b823998
labmember <labmember@kerouac.csres.utexas.edu>
2016-11-14 22:29:47 -0600

    LfD/Project: Debugging needed

--- LfD/Project/code/exp2_optim.m
+++ LfD/Project/code/exp2_optim.m
@@ -1,5 +1,5 @@
 % Direct Transcription
-clear
+% clear
 close
 clc
 
@@ -8,8 +8,8 @@ nInput = 2;
 nOutput = 2;
 nCov = nState^2;
 
-T = 50;
-del = 10;
+T = 5;
+del = 1;
 k = round(T/del);
 
 m_dash = rand(nState,1,k);
@@ -31,10 +31,9 @@ u_dash(:,:,1) = u;
 A = [1 0; 0 1];
 B = [1 0; 0 1];
 C = [1 0; 0 1];
-Q = 0.1*eye(nState);
-R = 1.0*eye(nInput);
-Q_f = 500*eye(nState);
-labda = 2000;
+Q = 0.5*eye(nState);
+R = 0.5*eye(nInput);
+labda = 200;
 W = 0.5*(5.0-m(1))^2*eye(nOutput);
 
 %Constraints
@@ -44,19 +43,42 @@ end
 m_dash(:,:,end) = goal;
 
 % x0 = [reshape(m_dash(:,:,1:end),[(k)*nState,1]);reshape(sig_dash(1,1,:),[k,1]);reshape(u_dash,[k*nInput,1])];
-x0 = rand(k*(nState + nInput + 1),1);
+x0 = zeros(k*(nState + nInput + 1),1) - 0.25;
+% x0 = xfinal;
 
 
 % Defining fmincon
 % % % % % optimization vector := [m1,.., mk [nState each], s, u1,..,uk [nInput each]]
 opti_A = [];
 opti_B = [];
-% opti_Aeq = [[eye(k*(nState+1)), zeros(k*(nState+1),nInput*k)]; zeros(k*nInput,k*(nState+1+nInput))];
-opti_Aeq = [zeros(nState,k*(nState+1+nInput));zeros((k-1)*nState,nState),eye((k-1)*nState),zeros((k-1)*nState,(k+1)*nInput);...
-    zeros(k,k*nState+1),[zeros(1,k-1);eye(k-1)],zeros(k,k*nInput);zeros(nInput,k*(nState+1+nInput))];
-opti_Beq = [m;reshape(m_dash(:,:,1:end-1),[(k-1)*nState,1]);reshape(sig_dash(1,1,:),[k,1]);zeros(k*nInput,1)];
-options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
-[xfinal fval exitflag] = fmincon(@(x)obj_fn(x,k,Q,R,labda),x0,opti_A,opti_B,opti_Aeq,opti_Beq,options)
+opti_Aeq = [eye(nState), zeros(nState,(k-1)*nState+k*(1+nInput));zeros((k-1)*nState,nState),eye((k-1)*nState),zeros((k-1)*nState,k*(nInput+1));...
+    zeros(k,k*nState),eye(k),zeros(k,k*nInput);zeros(k*nInput,k*(nState+1+nInput))];
+opti_Beq = [reshape(m_dash(:,:,1:end),[k*nState,1]);reshape(s_dash(:,1),[k,1]);zeros(k*nInput,1)];
+
+lb = -50*ones(size(x0,1),1);
+ub = 50*ones(size(x0,1),1);
+options = optimoptions('fmincon','Display','iter','Algorithm','sqp','ConstraintTolerance',1e-12);
+% [xfinal,fval,exitflag] = fmincon(@(x)obj_fn(x,k,Q,R,labda),x0,opti_A,opti_B,opti_Aeq,opti_Beq,lb,ub,[],options);
+[xfinal,fval,exitflag] = fmincon(@(x)obj_fn(x,k,Q,R,labda),x0,opti_A,opti_B,opti_Aeq,opti_Beq,lb,ub,[]);
+
+
+
+
+%%%%%%%%%%%Visualization%%%%%%%%%
+traj = [];
+for i=1:2:k*nState
+   traj = [traj, [xfinal(i,1);xfinal(i+1,1)]]; 
+end
+
+figure(1);clf;
+hold on
+plot(traj(1,1:end-1),traj(2,1:end-1),'r--','LineWidth',2);
+% scatter(traj(1,:),traj(2,:));
+plot(traj(1,1),traj(2,1),'mo', 'markers',12);
+plot(traj(1,end-1),traj(2,end-1),'gx','markers',12);
+hold off
+
+traj
 
 %     s_dash = x(2*k+1:3*k);
 %     for i=1:2:2*k
@@ -65,6 +87,7 @@ options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 %         sig_dash(:,:,ceil(i/2)) = s_dash(ceil(i/2))*eye(2,2);
 %     end
 % end
+
 
 
 
