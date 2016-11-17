89eb36bd3658bb138df6d17d5d3270ba55e29256
labmember <labmember@kerouac.csres.utexas.edu>
2016-11-16 19:58:13 -0600

    LfD/Project: Debugging needed

--- LfD/Project/code/finiteLQR.m
+++ LfD/Project/code/finiteLQR.m
@@ -1,16 +1,16 @@
-function [K,S] = finiteLQR(tf,A,B,Q,R,F,t_res)
+function [K,S] = finiteLQR(t_f,A,B,Q,R,F,t_res)
 nState = size(A,1);
 nInput = size(R,1);
-S = zeros(nState,nState,tf/t_res);
-S(:,:,tf)= F;
-K = zeros(nInput,nState,tf/t_res);
+S = zeros(nState,nState,t_f/t_res);
+S(:,:,t_f)= F;
+K = zeros(nInput,nState,t_f/t_res);
 
-for t=tf-1:-1:1
+for t=t_f-1:-1:1
     % Backward Pass
     S(:,:,t)= Q + A'*S(:,:,t+1)*A - A'*S(:,:,t+1)*B*((B'*S(:,:,t+1)*B+ R)\(B'*S(:,:,t+1)*A));
 end
 
-for t= 1:tf
+for t= 1:t_f
     K(:,:,t) = (B'*S(:,:,t)*B + R)\(B'*S(:,:,t)*A);
 end
 end
