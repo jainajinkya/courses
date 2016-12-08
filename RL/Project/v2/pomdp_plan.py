4f60e5fbdf7bea599153284c7f269b2d27f3f39f
Ajinkya <jainajinkya92@gmail.com>
2016-12-07 18:43:56 -0600

    Tiger Problem Working

--- RL/Project/v2/pomdp_plan.py
+++ RL/Project/v2/pomdp_plan.py
@@ -17,9 +17,9 @@ class  POMDP:
         self.nObservations = 2
         self.nActions = 3
         
-#        self.states = ['tiger_left', 'tiger_right']
-#        self.actions = ['listen', 'open_left', 'open_right']
-#        self.observations = ['tiger_left', 'tiger_right']
+        self.state = ['tiger_left', 'tiger_right']
+        self.action = ['listen', 'open_left', 'open_right']
+        self.obs = ['tiger_left', 'tiger_right']
         self.listen_success_prob = 0.85
         
         self.cur_state = 0
@@ -99,7 +99,7 @@ class  POMDP:
         if state == 0:
             return 0
         else:
-            return [st for st in range(1,3)
+            return [st for st in range(2)
                     if st != state][0]        
 
 
@@ -143,14 +143,6 @@ class  POMDP:
                 return 0.15
             else: 
                 return 0.85
-                  
-
-    def policy(self):
-        idx = random.randint(pomdp.nActions)
-        return idx
-        
-    
-        
 
 class algorithm():
     def __init__(self,nTraj,nT,pomdp,init_alpha,init_actions_alpha):
@@ -179,7 +171,8 @@ class algorithm():
             state = random.randint(self.pomdp.nStates)  # randomly choose a state out of nStates
             
             for j in range(self.nT):
-                action = self.pomdp.policy()
+                # Take a random action
+                action = random.randint(self.pomdp.nActions) 
 
                 # Draw a sample from the new probability distribution
                 new_state = self.pomdp.stateUpdate(state,action)
@@ -191,7 +184,7 @@ class algorithm():
                 b_a_z = self.beliefUpdate(b,action,obs)
 
                 # Add belief to the belief Set
-                self.B[i,j,:] = b_a_z
+                self.B[i,j,:] = b_a_z ## You need to store the belief State, action and observations as well
 
                 # Update for the new loop
                 b = b_a_z
@@ -210,10 +203,12 @@ class algorithm():
 
         
     def valueIteration(self):
-        k = 0
-        while(k<6): # Check if this condition for checking is correct or not
+        k = 1
+#        r = 1
+        while(k<10): # Check if this condition for checking is correct or not
             # Randomly choose a trajectory
             r = random.randint(0,self.nTraj)
+            
             k = k+1
             for t in range(self.nT-1,-1,-1):
                 old_value = self.beliefValue(self.B[r,t])
@@ -232,9 +227,10 @@ class algorithm():
                     # projecting to smaller representation: fit a K parameter GMM
                     # Check if direct implemetation like this works or do we need to generate samples before we can fit
                     # K-means gaussians
-                    gmix = mixture.GMM(n_components=self.K, covariance_type='full')
-                    gmix.fit(self.valueFn)
-                    self.valueFn_gmm = gmix.means_
+#                    gmix = mixture.GMM(n_components=self.K, covariance_type='full')
+#                    gmix.fit(self.valueFn)
+#                    self.valueFn_gmm = gmix.means_
+
 
     def beliefValue(self,b):
         if self.nAlphas == 1:
@@ -246,62 +242,90 @@ class algorithm():
 #        return dot(new_alpha,belief)
 
     
+#    def valueFnBackup(self,b):
+#        new_alpha = zeros(self.pomdp.nStates)
+#        action_alpha = zeros(self.pomdp.nStates)
+#        for i in range(self.pomdp.nStates):
+#            new_alpha[i] = max([self.max_a_R(i,j) + self.gamma*self.max_alpha_azj(i,j) for j in range(self.pomdp.nActions)])
+#            action_alpha[i] = argmax([self.max_a_R(i,j) + self.gamma*self.max_alpha_azj(i,j) for j in range(self.pomdp.nActions)])
+#        return new_alpha, action_alpha, dot(new_alpha,b)
+#        
+#    def max_a_R(self,state,action):
+#        return self.pomdp.rewardFunction(state,action)
+#        
+#    def max_alpha_azj(self,state,action):
+#        if self.nAlphas ==1:
+#            return sum([max([sum([dot(self.valueFn[:],s_dash)*self.pomdp.observationProb(s_dash,z)\
+#                 *self.pomdp.stateTransitionProbState(s_dash,state,action) for s_dash in range(self.pomdp.nStates)]) \
+#                 for j in range(self.nAlphas)]) for z in range(self.pomdp.nObservations)])
+#
+#        else:
+#            return sum([max([sum([dot(self.valueFn[j,:],s_dash)*self.pomdp.observationProb(s_dash,z)\
+#                 *self.pomdp.stateTransitionProbState(s_dash,state,action) for s_dash in range(self.pomdp.nStates)]) \
+#                 for j in range(self.nAlphas)]) for z in range(self.pomdp.nObservations)])
+    
     def valueFnBackup(self,b):
         new_alpha = zeros(self.pomdp.nStates)
-        action_alpha = zeros(self.pomdp.nStates)
-        for i in range(self.pomdp.nStates):
-            new_alpha[i] = max([self.max_a_R(i,j) + self.gamma*self.max_alpha_azj(i,j) for j in range(self.pomdp.nActions)])
-            action_alpha[i] = argmax([self.max_a_R(i,j) + self.gamma*self.max_alpha_azj(i,j) for j in range(self.pomdp.nActions)])
-        return new_alpha, action_alpha, dot(new_alpha,b)
+        new_V = max([dot(self.max_a_R(act),b) + dot(self.max_alpha_azj(act),b) for act in range(self.pomdp.nActions)])
+        opt_action = argmax([dot(self.max_a_R(act),b) + dot(self.max_alpha_azj(act),b) for act in range(self.pomdp.nActions)])
+        new_alpha = add(self.max_a_R(opt_action),self.max_alpha_azj(opt_action))
+        return new_alpha, opt_action, new_V
         
-    def max_a_R(self,state,action):
-        return self.pomdp.rewardFunction(state,action)
+    def max_a_R(self,action):
+        return [self.pomdp.rewardFunction(state,action) for state in range(self.pomdp.nStates)]
         
-    def max_alpha_azj(self,state,action):
-        if self.nAlphas ==1:
-            return sum([max([sum([dot(self.valueFn[:],s_dash)*self.pomdp.observationProb(s_dash,z)\
-                 *self.pomdp.stateTransitionProbState(s_dash,state,action) for s_dash in range(self.pomdp.nStates)]) \
-                 for j in range(self.nAlphas)]) for z in range(self.pomdp.nObservations)])
-
+    def max_alpha_azj(self,action):
+        if self.nAlphas == 1:
+            return [self.gamma*sum([max([sum([self.valueFn[s_dash]*self.pomdp.observationProb(s_dash,obs)*self.pomdp.stateTransitionProbState(s_dash,state,action) for s_dash in range(self.pomdp.nStates)]) for j in range(self.nAlphas)]) for obs in range(self.pomdp.nObservations)]) for state in range(self.pomdp.nStates)]
         else:
-            return sum([max([sum([dot(self.valueFn[j],s_dash)*self.pomdp.observationProb(s_dash,z)\
-                 *self.pomdp.stateTransitionProbState(s_dash,state,action) for s_dash in range(self.pomdp.nStates)]) \
-                 for j in range(self.nAlphas)]) for z in range(self.pomdp.nObservations)])
-    
-    def plan(self,state,belief):
-        while(self.run):
-            alpha_max = argmax([dot(self.valueFn[j,:],belief) for j in range(self.nAlphas)])
-            action_plan = self.actions_alpha[alpha_max,:]
-            new_state = self.pomdp.stateUpdate(state,action_plan[state])
-            if action_plan[state] != 0:
-                self.run = False
-            state = new_state
-            return new_state
+            return [self.gamma*sum([max([sum([self.valueFn[j][s_dash]*self.pomdp.observationProb(s_dash,obs)*self.pomdp.stateTransitionProbState(s_dash,state,action) for s_dash in range(self.pomdp.nStates)]) for j in range(self.nAlphas)]) for obs in range(self.pomdp.nObservations)]) for state in range(self.pomdp.nStates)]
+
+
+    def plan(self,b_init,n_eps):        
+        for i in range(n_eps):
+            print "Starting New Episode #########################"
+            state = random.randint(self.pomdp.nStates)
+            print "state =", self.pomdp.state[state]
+            belief = b_init
+            self.run = True
+          
+            while(self.run):
+                print "Current belief = ", belief          
+                alpha_max = argmax([dot(self.valueFn[j],belief) for j in range(self.nAlphas)])
+                action = self.actions_alpha[alpha_max]
+                print "action = ", self.pomdp.action[int(action)]
+                obs = self.pomdp.observations(state,action)
+                belief = self.beliefUpdate(belief,action,obs)
+                print "observation = ", self.pomdp.obs[int(obs)]                
+                print "\n"
+                
+                if int(action) != 0:
+                    self.run = False         
+            
+        return
             
         
 
 if __name__ == "__main__":
-    nEps = 2
+    nEps = 5
     nTraj = 20
     nT = 3
     b_init = array([0.5, 0.5])
     alpha_init = array([0.0, 0.0])
-    init_actions_alpha = array([0, 0])
-#    B_set = [b_init]
-       
-#    environment = TigerProblem()
+    init_actions_alpha = array([0])
+
     pomdp = POMDP()
     pomdp_solve = algorithm(nTraj,nT,pomdp, alpha_init,init_actions_alpha)
-#    vi = VI(alpha,nTraj,nT)
     
     pomdp_solve.generateBeliefSet(b_init)
     pomdp_solve.valueIteration()
-#    
+    pomdp_solve.plan(b_init,nEps)
 #    state = 0
 #    pomdp_solve.run = True
 #    new_state, beleif = pomdp_solve.plan(state,b_init)
-    print "Beleif Set = ", pomdp_solve.B
-    print "Value function =", pomdp_solve.valueFn
+#    print "Beleif Set = ", pomdp_solve.B
+#    print "Value function =", pomdp_solve.valueFn
+#    print traj
 #        environment.reset()
         
 
